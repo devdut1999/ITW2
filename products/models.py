b55e2cf3ea5e581d8850d9e257bb909e495912b4
Devadutta <devadutta.dash.cd.cse19@itbhu.ac.in>
2020-11-14 13:49:20 +0530

    Final working model

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    url = models.TextField()
+    url = models.URLField()
     image = models.ImageField(upload_to='images/')
     icon = models.ImageField(upload_to='images/')
     votes_total = models.IntegerField(default=1)
